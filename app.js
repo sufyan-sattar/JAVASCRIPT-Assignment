0a17266119396bedc65e99fd95cb939ea3ce9ddb
sufyan-sattar <sufyanqazi36@gmail.com>
2023-09-01 15:01:43 +0500

    Javascript Assignment

---
+++ app.js
@@ -0,0 +1,61 @@
+                        //    Chapter (1)
+
+// Question 1:-
+alert("Assalam Alaikum!")
+
+// Question 2:-
+alert("please! Enter a Valid password.")
+
+// Question 3:-
+alert("Welcome to JSland... \nHappy Coding!")
+
+// Question 4:-
+alert("Welcome to JSLand...")
+alert ("Happy Coding!\n"
++"prevent this page from creating additionals dialogs")
+
+
+                            //  Chapter (2)
+
+// Questio 1:-
+var name1= "Username:"
+console.log (name1)
+
+// Question 2:-
+var name2= "Sufyan Qazi"
+console.log (name2)
+
+// Question 3:-
+var message = ("Hello World")
+alert (message)
+
+// Question 4:-
+var data1 = ("Sufyan Qazi")
+alert (data1)
+
+var data2 = ("19 years old")
+alert (data2)
+
+var data3 = ("Certified Mobile Application Development")
+alert (data3)
+
+// Question 5:-
+var pizza = (" PIZZA\n PIZZ\n PIZ\n PI\n P\n")
+alert (pizza)
+
+// Question 6:-
+var email = ("sufyanqazi36@gmail.com")
+alert ('my email address is' + email)
+
+// Question 7:-
+var learn = ("I am trying to learn from the book A Smarter way to learn JAVASCRIPT")
+alert (learn)
+
+// Question 8:-
+document.write ("Assalam Alaikum! My name is Sufyan and i can write HTML content through JAVASCRIPT")
+
+
+// Question 9:-
+var unique = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+alert (unique)
+document.write ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
